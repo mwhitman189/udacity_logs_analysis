ee94acacae1ddfca44370766d8c512c9e31137b2
mWhitman189 <mwhitman189@gmail.com>
2018-06-23 20:14:25 +0900

    Create reporting tool .py file

---
+++ internal-reporting-tool.py
@@ -0,0 +1,4 @@
+# Internal reporting tool for use with news website PostgeSQL databaseself. #This programs provides data on:
+#    1) The 3 most popular articles of all time
+#    2) Who the most popular articles are written by
+#    3) On which days more than 1% of requests led to errors.
